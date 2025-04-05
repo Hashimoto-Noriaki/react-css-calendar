9f95036e233b8810822f5eacb8071a31138244bc
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-04-05 22:55:14 +0900

    トップ表示

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import './App.css' 
 
 export const App = () => {
   return (
-    <div>
-      <h1>
+    <div class="container">
+      <h1 class="logo">
         App
       </h1>
     </div>
